e66c536cb7acdeba26c70a0ba346f676134b340c
bharani2624 <bharani2624baru@gmail.com>
2024-11-20 21:05:34 +0530

    Declared Variables And Learned About Match(Control Flow)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io:{self,Read,Write};
+use std::net::{TcpListener,TcpStream};
+use std::path:Path;
+
+#[no_mangle]
+
+pub extern "C" fn send(file_path: *const i8, address: *const i8) -> i32
+{
+    let c_file_path=unsafe{std::ffi::CStr::from_ptr(file_path)};//unsafe because raw pointers are unsafe inherently
+    let c_address=unsafe{std::ffi:Cstr::from_ptr(address)};
+    let file_path=match c_file_path.to_str()
+    {
+        Ok(path) => path,
+        Err(_) => return -1,
+    };
+    let address=match c_address.to_str()
+    {
+        Ok(addr)=>addr,
+        Err(_)=>return -1,
+    };
+    
 }
